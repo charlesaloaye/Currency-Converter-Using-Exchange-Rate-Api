1d1c4339c7237197c2277efac48c7ce195f11441
Sedenu Aloaye Charles <aloayecharles@gmail.com>
2021-02-25 11:59:11 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let price = 700;
+    let fromCurrency = 'USD';
+    let toCurrency = "EUR";
+    
+				fetch('https://api.exchangeratesapi.io/latest?base=' + fromCurrency)
+				.then(response => response.json(),)
+				.then(data => {
+                
+					console.log(data);
+					const rate = Math.round(data.rates.EUR, 4);
+					document.querySelector('body').innerHTML = `${price} ${fromCurrency} = ${rate} ${toCurrency}. `;
+
+				});
+			});
